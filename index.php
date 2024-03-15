7ddd40c8b741b48f9da6f916ece821118771482b
Peaumm <maxence.fouquet@gmail.com>
2024-03-15 11:00:04 +0100

    feat router

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
 
 require 'vendor/autoload.php';
+
+$availablesRoutes = 
+['homepage',
+];
+
+$route = 'homepage';
+
+if (isset($_GET['page']) && in_array($_GET['page'],$availablesRoutes)) {
+    $route = $_GET['page'];
+}
